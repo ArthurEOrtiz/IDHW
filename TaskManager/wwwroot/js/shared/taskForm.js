22370c1e8245ab56872626d03f40b618283de592
Arthur Ortiz <Ortiz.Arthur.E@gmail.com>
2024-05-03 21:25:58 -0600

    character counter
    The character counter now works through out the application

--- TaskManager/wwwroot/js/shared/taskForm.js
+++ TaskManager/wwwroot/js/shared/taskForm.js
@@ -1,11 +1,15 @@
-﻿const limit = 500
-const textArea = document.getElementById('Description');
-const counter = document.getElementById('descriptionCounter');
+﻿(() => {
+  const limit = 500;
+  const textAreas = document.querySelectorAll('.description');
+  const counters = document.querySelectorAll('.descriptionCounter');
 
-// Set the initial count
-counter.innerText = `${textArea.value.length}/${limit}`;
+  textAreas.forEach((textArea, index) => {
+    // Set the initial count
+    counters[index].innerText = `${textArea.value.length}/${limit}`;
 
-// Update the count when the user types
-textArea.addEventListener('input', () => {
-  counter.innerText = `${textArea.value.length}/${limit}`;
-});
+    // Update the count when the user types
+    textArea.addEventListener('input', () => {
+      counters[index].innerText = `${textArea.value.length}/${limit}`;
+    });
+  });
+})();
