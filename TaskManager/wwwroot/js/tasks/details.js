3dc4f0fb610c78f6d2973a3100d44e24b7b1e8e7
Arthur Ortiz <Ortiz.Arthur.E@gmail.com>
2024-05-06 14:13:16 -0600

    Update details.js

--- TaskManager/wwwroot/js/tasks/details.js
+++ TaskManager/wwwroot/js/tasks/details.js
@@ -1,31 +1,33 @@
-﻿openEditModal = (id) => {
-  fetch('/Tasks/EditModal/' + id)
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('editModalContainer').innerHTML = data;
-      const editModalElement = document.getElementById('editModal');
-      const editFormElement = document.getElementById('editForm');
-      const editModal = new bootstrap.Modal(editModalElement);
+﻿(() => { 
+  openEditModal = (id) => {
+    fetch('/Tasks/EditModal/' + id)
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('editModalContainer').innerHTML = data;
+        const editModalElement = document.getElementById('editModal');
+        const editFormElement = document.getElementById('editForm');
+        const editModal = new bootstrap.Modal(editModalElement);
 
-      editFormElement.addEventListener('submit', function (e) {
-        e.preventDefault();
-        const formData = new FormData(this);
-        fetch('/Tasks/Edit', {
-          method: 'POST',
-          body: formData
-        })
-          .then(response => response.json())
-          .then(data => {
-            if (data.success) {
-              editModal.hide();
-              location.reload();
-            } else {
-              alert('Error: ' + data.errors.join('\n'));
-            }
-          });
+        editFormElement.addEventListener('submit', function (e) {
+          e.preventDefault();
+          const formData = new FormData(this);
+          fetch('/Tasks/Edit', {
+            method: 'POST',
+            body: formData
+          })
+            .then(response => response.json())
+            .then(data => {
+              if (data.success) {
+                editModal.hide();
+                location.reload();
+              } else {
+                alert('Error: ' + data.errors.join('\n'));
+              }
+            });
+        });
+
+        editModal.show();
       });
-
-      editModal.show();
-    });
-}
+  }
+})();
 
